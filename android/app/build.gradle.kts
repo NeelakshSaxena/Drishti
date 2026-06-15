bf7227ce51106df4b7765288247db672fd470fb6
Neelaksh Saxena <87892838+NeelakshSaxena@users.noreply.github.com>
2026-06-15 11:28:29 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.drishti.node"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.drishti.node"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.1.0"
 
